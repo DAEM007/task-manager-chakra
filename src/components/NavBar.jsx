e5ae32f78f393bffccf38a0b319396270aebfe84
Emmanuel <dammylola299@gmail.com>
2023-05-08 19:20:20 +0100

    updated navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Box, Button, Flex, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
+
+
+export default function NavBar() {
+  return (
+    <Flex as="nav" p="10px" alignItems="center">
+        <Heading as="h1">Daem Tasks</Heading>
+        <Spacer />
+        <HStack spacing="20px">
+            <Box bg="gray.200" p="10px">A</Box>
+            <Text>emmanuel@gmail.com</Text>
+            <Button colorScheme="purple">Logout</Button>
+        </HStack>
+    </Flex>
+  )
+}
