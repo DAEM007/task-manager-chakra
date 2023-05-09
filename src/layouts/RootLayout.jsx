bd9a65c577f67bf0199cd1b651164731e9819758
Emmanuel <dammylola299@gmail.com>
2023-05-09 13:19:47 +0100

    updated root layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,29 @@
 import { Outlet } from "react-router-dom"
+import NavBar from "../components/NavBar"
+import { Grid, GridItem } from "@chakra-ui/react"
+import SideBar from "../components/SideBar"
 
 export default function RootLayout() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      <GridItem 
+        as="aside"
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        bg="purple.400"
+        minHeight={{ lg: "100vh" }}
+        p="30px"
+      >
+        <SideBar />
+      </GridItem>
+      <GridItem
+        as="main"
+        colSpan={{ base: 6, lg: 4, xl: 5 }}
+        p="40px"
+      >
+        <NavBar />
+        <Outlet />  
+      </GridItem>
+    </Grid>
   )
 }
+
