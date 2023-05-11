0e625ddcb8d827ab5f02b6731a18bd73f766e8e4
Emmanuel <dammylola299@gmail.com>
2023-05-11 19:22:31 +0100

    updated profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,77 @@
+import { 
+  ChatIcon,
+  EmailIcon,
+  StarIcon,
+  CheckCircleIcon,
+  WarningIcon
+} from "@chakra-ui/icons";
+
+import { 
+  List,
+  ListIcon,
+  ListItem,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs
+} from "@chakra-ui/react";
+
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <Tabs p="20px" mt="40px" variant="enclosed">
+      <TabList>
+        <Tab _selected={{ bg: 'purple.400', color: 'white' }}>Account Info</Tab>
+        <Tab _selected={{ bg: 'purple.400', color: 'white' }}>Task History</Tab>
+      </TabList>  
+
+      <TabPanels py="10px">
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={EmailIcon} />
+              email: damiemmanuel1000@gmail.com
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon} />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, labore.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={StarIcon} />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, sunt?
+            </ListItem>
+          </List>
+        </TabPanel>
+        <TabPanel>
+        <List spacing={4}>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color="red.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color="teal.400" />
+              Lorem ipsum dolor sit amet consectetur.
+            </ListItem>
+          </List>
+        </TabPanel>
+      </TabPanels>
+
+    </Tabs>
   )
 }
