8edc66754b040bf27ba77569d49e17c785f57ac3
Emmanuel <dammylola299@gmail.com>
2023-05-12 16:33:25 +0100

    updated the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,45 @@
-import { Box, Button, Flex, HStack, Heading, Spacer, Text } from "@chakra-ui/react";
-
+import { LockIcon } from "@chakra-ui/icons";
+import { 
+  Avatar,
+  AvatarBadge,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  Spacer,
+  Text,
+  useToast
+} from "@chakra-ui/react";
 
 export default function NavBar() {
+  const toast = useToast();
+  
+  const showToast = () => {
+    // console.log('Toast alive!');
+    toast({
+      title: 'Logged Out!',
+      description: `You've just logged out!`,
+      status: 'success',
+      duration: 3000,
+      isClosable: true,
+      position: 'top',
+      icon: <LockIcon />,
+      colorScheme: 'purple'
+    })
+  }
+
   return (
     <Flex as="nav" p="10px" mb="40px" alignItems="center">
         <Heading as="h1">Daem Tasks</Heading>
         <Spacer />
         <HStack spacing="20px">
-            <Box bg="gray.200" p="10px">A</Box>
+            <Avatar src="/img/richard.avif">
+              <AvatarBadge boxSize="1.25em" bg="teal.500">
+                <Text fontSize="xs">5</Text>
+              </AvatarBadge>
+            </Avatar>
             <Text>emmanuel@gmail.com</Text>
-            <Button colorScheme="purple">Logout</Button>
+            <Button colorScheme="purple" onClick={ () => showToast() }>Logout</Button>
         </HStack>
     </Flex>
   )
